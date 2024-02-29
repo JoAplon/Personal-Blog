6401c665e72685f492d48ddba3f12ccdee900cb6
JoAplon <158791447+JoAplon@users.noreply.github.com>
2024-02-28 17:33:42 -0800

    dark and light theme working

--- assets/logic.js
+++ assets/logic.js
@@ -1,1 +1,17 @@
+const container = document.querySelector('html');
+const themeButton = document.querySelector('#theme-toggle');
 
+let mode = 'lighttheme';
+
+themeButton.addEventListener('click', function() {
+    if (mode === 'lighttheme') {
+        mode = 'darktheme';
+        container.setAttribute('class', 'darktheme');
+        themeButton.textContent = '🌙';
+    }
+    else{
+        mode = 'lighttheme';
+        container.setAttribute('class', 'lighttheme');
+        themeButton.textContent = '☀️';
+    }
+});
